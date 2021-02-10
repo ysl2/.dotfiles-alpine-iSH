1db10be6236ed194ac689c165aec4fe44200d959
ysl2 <songli.yu@outlook.com>
2021-02-10 08:46:46 +0000

    2021-02-10 08:46:46

--- my-git-upload.sh
+++ my-git-upload.sh
@@ -1,5 +1,5 @@
 # 用于上传dotfiles
 #----------------------------------------
 
-git add . > /dev/null && git commit -m "$(date +"%Y-%m-%d %H:%M:%S")" > /dev/null && git push origin master -q > /dev/null
+git add . > /dev/null && git commit -m "$(date +"%Y-%m-%d %H:%M:%S")" > /dev/null && git push origin master
 
